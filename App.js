f3392e31d93b45a73d39d2e07c6e7bccaf485332
Hwang Daseul <darr880406@mindmap.ai>
2018-03-09 10:57:42 +0900

    init

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// http://twinw.tistory.com/184 node 업그레이드
+//
+
+class App extends React.Component {
+    render(){
+
+        return (
+                <h1>Hello React Skeleton</h1>
+        );
+    }
+}
+
+export default App;
